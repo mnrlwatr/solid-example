1bee44c516758ccd9a67a2effcbfe02eb6dde25c
Adm Syd <adm_syd@icloud.com>
2024-10-30 18:24:38 +0500

    added Open Closed Principle example

--- src/main/java/ocp/without/AreaCalculator.java
+++ src/main/java/ocp/without/AreaCalculator.java
@@ -1,12 +1,21 @@
 package ocp.without;
 
 public class AreaCalculator {
+    
+    public static double calculateArea(Object object) {
+        if (object instanceof Rectangle){
+            return calculateRectangleArea((Rectangle) object);
+        } else if (object instanceof Circle){
+            return calculateCircleArea((Circle) object);
+        }
+        return 0.0;
+    }
 
-    public static double calculateRectangleArea(Rectangle rectangle) {
+    private static double calculateRectangleArea(Rectangle rectangle) {
         return rectangle.getLength() * rectangle.getWidth();
     }
 
-    public static double calculateCircleArea(Circle circle) {
+    private static double calculateCircleArea(Circle circle) {
         return Math.PI * Math.pow(circle.getRadius(), 2);
     }
 
