50789f295e2c1b7b8867026cf87f249eac4559ea
Adm Syd <adm_syd@icloud.com>
2024-11-05 18:42:38 +0500

    added some rules to create well-behaved subtypes

--- src/test/java/Child.java
+++
@@ -1,7 +0,0 @@
-public class Child  extends Parent{
-
-    @Override
-    Integer someM2(Integer inte) throws Throwable{
-        return null;
-    }
-}
--- src/test/java/Parent.java
+++
@@ -1,7 +0,0 @@
-public class Parent {
-
-    Object someM2(Integer inte) throws NullPointerException {
-        System.out.println("BCDDA");
-        return 0;
-    }
-}
