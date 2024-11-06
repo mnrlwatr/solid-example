11f0b5671cc5328a63adb8239fc692975ac4c669
Adm Syd <adm_syd@icloud.com>
2024-11-07 00:42:29 +0500

    added Interface Segregation Principle example

--- src/main/java/isp/with/Player.java
+++ src/main/java/isp/with/Player.java
@@ -1,5 +1,18 @@
 package isp.with;
 
 public class Player implements Attacker,ItemPicker,Mover {
+    @Override
+    public void attack() {
 
+    }
+
+    @Override
+    public void pickItem() {
+
+    }
+
+    @Override
+    public void mover() {
+
+    }
 }
