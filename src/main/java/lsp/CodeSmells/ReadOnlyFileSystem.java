65532e1dff66b0b1b5bfeadc87f4752df5328546
Adm Syd <adm_syd@icloud.com>
2024-12-10 19:38:16 +0500

    comment fix

--- src/main/java/lsp/CodeSmells/ReadOnlyFileSystem.java
+++ src/main/java/lsp/CodeSmells/ReadOnlyFileSystem.java
@@ -12,7 +12,7 @@ public class ReadOnlyFileSystem implements FileSystem {
     @Override
     public void deleteFile(String path) throws IOException {
         // A Subtype Provides No Implementation (do nothing) for a Behavior It Can’t Fulfill
-        // or throws an exception for a Behavior It Can’t Fulfill
+        // or throws an exception for a Behavior It Can’t Fulfill (throw new UnsupportedOperationException)
         // or always Returns the Same Fixed Value (e.g. return 0;)
     }
 
