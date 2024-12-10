016ed4859cdbe640b4faa299b47b55b924744544
Adm Syd <adm_syd@icloud.com>
2024-12-10 19:00:44 +0500

    added missed @Override

--- src/main/java/lsp/CodeSmells/ReadOnlyFileSystem.java
+++ src/main/java/lsp/CodeSmells/ReadOnlyFileSystem.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class ReadOnlyFileSystem implements FileSystem {
+    @Override
     public File[] listFiles(String path) {
         // code to list files
         return new File[0];
     }
-
+    @Override
     public void deleteFile(String path) throws IOException {
         // A Subtype Provides No Implementation (do nothing) for a Behavior It Can’t Fulfill
         // or throws an exception for a Behavior It Can’t Fulfill
         // or always Returns the Same Fixed Value (e.g. return 0;)
     }
+
+
 }
